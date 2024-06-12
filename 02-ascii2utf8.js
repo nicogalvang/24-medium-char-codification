4f32af2b33ea6be2280c150470f0973973788344
nicogalvang <nico.galvangarcia@gmail.com>
2024-06-12 13:17:19 +0200

    Muestra los caracteres A, B, C y D

--- 02-ascii2utf8.js
+++ 02-ascii2utf8.js
@@ -3,3 +3,8 @@ function asciiAUtf8(caracter) {
     let utf8Hex = codigoAscii.toString(16);
     return utf8Hex;
 }
+
+console.log(asciiAUtf8("A"))
+console.log(asciiAUtf8("B"))
+console.log(asciiAUtf8("C"))
+console.log(asciiAUtf8("D"))
